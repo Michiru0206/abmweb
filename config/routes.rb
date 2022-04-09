509b639450a1238c69252f54a26ffab5bf26bd79
Michiru Desktop <0126michiru@gmail.com>
2022-04-09 19:21:58 -0400

    Update routes.rb
    change root route from todos#index to home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
   devise_for :users
   # route routing 
-  root to: "todos#index"
+  root to: "home#index"
 
   get 'diary/index'
   get 'todos/index'
+  get 'home/index'
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
