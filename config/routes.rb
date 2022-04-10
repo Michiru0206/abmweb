a9edaf025c422c2e0111f2f84a8328bb688d9515
Michiru Desktop <0126michiru@gmail.com>
2022-04-09 20:38:21 -0400

    Update routes.rb
    generate controller for users and added information to the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
   # route routing 
   root to: "home#index"
 
@@ -8,6 +13,7 @@ Rails.application.routes.draw do
   get 'todos/index'
   get 'home/index'
   
+  #get '/sign_in', to: 'todos#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
