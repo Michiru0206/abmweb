7a1bf5c2aaeb92066da64052c93e9edeccdc5a52
Michiru Desktop <0126michiru@gmail.com>
2022-04-11 16:23:31 -0400

    Delete 20220411201116_add_colums_to_todotable.rb
    remove the latest migration since it gives us an error...

--- db/migrate/20220411201116_add_colums_to_todotable.rb
+++
@@ -1,7 +0,0 @@
-class AddColumsToTodotable < ActiveRecord::Migration[6.1]
-  def change
-    add_column :todotables, :task, :text
-    add_column :todotables, :due_date, :date
-    add_column :todotables, :status, :text
-  end
-end
