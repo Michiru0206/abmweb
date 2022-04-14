4b44f1d11dbe7c743123a8408e8c104e41dbf0f7
Annah N Mutaya <amutaya@smith.edu>
2022-04-13 22:52:23 -0400

    Adds a new migration to associate different users to their tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,6 +65,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:task, :status, :due)
+      params.require(:task).permit(:task, :status, :due, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_12_030618) do
+ActiveRecord::Schema.define(version: 2022_04_14_023222) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "task"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2022_04_12_030618) do
     t.date "due"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
