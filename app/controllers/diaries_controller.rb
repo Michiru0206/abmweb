6d640b80244e1c3acfa6b37e15846df9219ae58e
Michiru Desktop <0126michiru@gmail.com>
2022-04-16 14:24:28 -0400

    Update diaries_controller.rb
    add basic functions to the diaries controller

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -12,8 +12,28 @@ class DiariesController < ApplicationController
     redirect_to diaries_path
   end
 
+  def edit
+    @diary = Diary.find(params[:id])
+  end
+
+  def update
+    @diary = Diary.find(params[:id])
+    @diary.update(diary_params)
+    redirect_to diaries_path
+  end
+
+  def destroy
+    @diary = Diary.find(params[:id])
+    @diary.destroy
+    redirect_to diaries_path
+  end
+
+  def show
+    @diary = Diary.find(params[:id])
+  end
+
   private
     def diary_params
-      params.require(:diary).permit(:writing_date, :title)
+      params.require(:diary).permit(:writing_date, :title, :content)
     end
 end
