d793050a519d427fa76a0f6c9e2e731c1ed7b18e
Michiru Desktop <0126michiru@gmail.com>
2022-04-16 14:22:09 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   resources :todos
   
-  #get '/sign_in', to: 'todos#index'
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
