f97b45e846a5e628d53dc702a95a1e94fdccb69d
Michiru Desktop <0126michiru@gmail.com>
2022-04-18 19:35:25 -0400

    Update diaries_controller.rb
    Associate diaries page with current user

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,8 +2,8 @@ class DiariesController < ApplicationController
   # before_action :set_diaries, only: [:show, :edit, :update]
   # before_action :authenticate_user!, except: [:index ]
   def index
-    #@diaries = current_user.diaries
-    @diaries = Diary.all
+    @diaries = current_user.diaries
+    #@diaries = Diary.all
   end
 
   def new
@@ -11,40 +11,40 @@ class DiariesController < ApplicationController
   end
 
   def create
-    #@diary = current_user.diaries.new(diary_params)
-    #@diary.save!
-    @diary = Diary.create(diary_params)
+    @diary = current_user.diaries.new(diary_params)
+    @diary.save!
+    #@diary = Diary.create(diary_params)
     redirect_to diaries_path
   end
 
   def edit
-    #@diary = target_diary params[:id]
-    @diary = Diary.find(params[:id])
+    @diary = target_diary params[:id]
+    #@diary = Diary.find(params[:id])
   end
 
   def update
-    #@diary = target_diary params[:id]
-    @diary = Diary.find(params[:id])
+    @diary = target_diary params[:id]
+    #@diary = Diary.find(params[:id])
     @diary.update(diary_params)
     redirect_to diaries_path
   end
 
   def destroy
-    #@diary = target_diary params[:id]
-    @diary = Diary.find(params[:id])
+    @diary = target_diary params[:id]
+    #@diary = Diary.find(params[:id])
     @diary.destroy
     redirect_to diaries_path
   end
 
   def show
-    #@diary = target_diary params[:id]
-    @diary = Diary.find(params[:id])
+    @diary = target_diary params[:id]
+    #@diary = Diary.find(params[:id])
   end
 
   private
-    # def target_diary diary_id
-    #   current_user.diaries.where(id: diary_id).take
-    # end
+    def target_diary diary_id
+      current_user.diaries.where(id: diary_id).take
+    end
 
     def diary_params
       params.require(:diary).permit(:writing_date, :title, :content)
