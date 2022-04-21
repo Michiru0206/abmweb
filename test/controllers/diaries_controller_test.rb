e6922943a4e6eb91444043ca36b2217e40d4c0e3
Michiru Desktop <0126michiru@gmail.com>
2022-04-20 20:56:17 -0400

    Update diaries_controller_test.rb

--- test/controllers/diaries_controller_test.rb
+++ test/controllers/diaries_controller_test.rb
@@ -1,8 +1,8 @@
-# require "test_helper"
+require "test_helper"
 
-# class DiariesControllerTest < ActionDispatch::IntegrationTest
-#   test "should get index" do
-#     get diaries_index_url
-#     assert_response :success
-#   end
-# end
+class DiariesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get diaries_index_url
+    assert_response :success
+  end
+end
