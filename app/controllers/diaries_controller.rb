29e25b25cef8e0514ca153284276485351985083
Michiru Desktop <0126michiru@gmail.com>
2022-04-23 19:13:41 -0400

    Update diaries_controller.rb
    added comments to diary controller

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,36 +1,45 @@
 class DiariesController < ApplicationController
   # before_action :set_diaries, only: [:show, :edit, :update]
   # before_action :authenticate_user!, except: [:index ]
+  
+  # List all diaries of current user
   def index
     @diaries = current_user.diaries
   end
 
+  # Create a new empty diary
   def new
     @diary = Diary.new
   end
 
+  # Create a diary which contains writing date, title, and content, and save 
+  # Redirect to diary page after the new diary is saved
   def create
     @diary = current_user.diaries.new(diary_params)
     @diary.save!
     redirect_to diaries_path
   end
 
+  # Edit the selected diary
   def edit
     @diary = target_diary params[:id]
   end
 
+  # Update the selected diary and redirect to diary page after it is updated
   def update
     @diary = target_diary params[:id]
     @diary.update(diary_params)
     redirect_to diaries_path
   end
 
+  # Delete the selected diary and redirect to diary page after it is destroied
   def destroy
     @diary = target_diary params[:id]
     @diary.destroy
     redirect_to diaries_path
   end
 
+  # Show the details of the diary - users can read content
   def show
     @diary = target_diary params[:id]
   end
