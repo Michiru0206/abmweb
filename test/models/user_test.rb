adbdb63efa36ce92bdcc94560792b0492eab3e72
Michiru Desktop <0126michiru@gmail.com>
2022-04-23 12:57:30 -0400

    Update user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # test "put email and password to create a new user" do
+    @user = User.new(email: "testuser@example.com", encrypted_password: "TestUser!")
+  #   assert true
+  #   #assert @user.invalid?
+  # end
 end
