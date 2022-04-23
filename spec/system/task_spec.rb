3e287c8de63bea76297b3d91b628a53287c5f899
Michiru Desktop <0126michiru@gmail.com>
2022-04-23 18:31:57 -0400

    Create task_spec.rb
    Unit test for task

---
+++ spec/system/task_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Sign up to use the app', type: :system do
+    before do
+        @task = FactoryBot.build(:task)
+        @user = FactoryBot.create(:user)
+    end
+
+    scenario 'User can add new task' do
+        # User sign in 
+        sign_in @user
+        # Go to new task page
+        visit tasks_path
+        click_on 'New Task', match: :first
+        visit new_task_path
+        # Fill in all the info to create a new task
+        fill_in('New task', with: @task.task)
+        fill_in('Status', with: @task.status)
+        #Test - click the button and the number of task increases by one
+        expect { click_on 'Create Task' }.to change(Task, :count).by(1)
+    end
+end
