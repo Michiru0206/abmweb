449db0a20fdfd48747b6c9956b2d863416321230
Michiru Desktop <0126michiru@gmail.com>
2022-04-23 18:31:44 -0400

    Create diary_spec.rb
    Unit test for diary

---
+++ spec/system/diary_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Sign in and create diary', type: :system do
+    before do
+        @diary = FactoryBot.build(:diary)
+        @user = FactoryBot.create(:user)
+    end
+
+    scenario 'User can add new diary' do
+        # User sign in
+        sign_in @user
+        visit tasks_path
+        click_on 'Diary'
+        visit diaries_path
+        click_on 'Write Today\'s Diary', match: :first
+        visit new_diary_path
+        # Fill in all the info to write a new diary
+        fill_in('Title', with: @diary.title)
+        fill_in('Content', with: @diary.content)
+        # Test
+        expect { click_on 'Create Diary' }.to change(Diary, :count).by(1)
+        #expect(current_path).to eq(diaries_path)
+    end
+end
