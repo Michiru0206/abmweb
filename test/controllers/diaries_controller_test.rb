3ac5a6fceaf2b697ee8d97b2ab6ede7db9d204f4
Michiru Desktop <0126michiru@gmail.com>
2022-04-23 12:57:20 -0400

    Update diaries_controller_test.rb

--- test/controllers/diaries_controller_test.rb
+++ test/controllers/diaries_controller_test.rb
@@ -1,8 +1,16 @@
-# require "test_helper"
+require "test_helper"
 
-# class DiariesControllerTest < ActionDispatch::IntegrationTest
-#   test "should get index" do
-#     get diaries_index_url
-#     assert_response :success
-#   end
-# end
+class DiariesControllerTest < ActionDispatch::IntegrationTest
+    setup do
+        @user = users(:testuser)
+        @diary = @user.diaries
+    end
+    # @user = User.new(email: "testuser@example.com", encrypted_password: "TestUser!")
+    # @diary = @user.diaries.new(writing_date: 2022-04-15, title: "Test Diary", content: "This is a test.")
+    # @diary.save!
+
+  test "should get index" do
+    get diaries_index_url
+    assert_response :success
+  end
+end
