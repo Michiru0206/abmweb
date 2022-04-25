c379f413352cf8f91b62883fb72211e02a8608aa
Michiru Desktop <0126michiru@gmail.com>
2022-04-24 21:54:53 -0400

    Update user_test.rb
    comment out test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,7 @@
-require "test_helper"
+# require "test_helper"
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
-  # test "put email and password to create a new user" do
-    @user = User.new(email: "testuser@example.com", encrypted_password: "TestUser!")
-  #   assert true
-  #   #assert @user.invalid?
-  # end
-end
+# class UserTest < ActiveSupport::TestCase
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
